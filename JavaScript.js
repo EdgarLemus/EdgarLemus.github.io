8ab13952303843c32bf4b00e227bc5f3b353c82e
EdgarLemus <31544878+EdgarLemus@users.noreply.github.com>
2017-11-17 00:32:47 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -17,23 +17,48 @@ function hora()
 
 var r = setInterval("hora()");
 
-$(function() {
-    $.ajax({
-      url: '//connect.facebook.net/es_ES/all.js',
-      dataType: 'script',
-      cache: true,
-      success: function() {
-        FB.init({
-          appId: '335324360274138',
-          xfbml: true
-        });
-        FB.Event.subscribe('auth.authResponseChange', function(response) {
-          if (response && response.status == 'connected') {
-            FB.api('/me', function(response) {
-              alert('Nombre: ' + data.name);
-            });
-          }
-        });
-      }
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '335324360274138',
+        cookie: true,  
+        xfbml: true,  
+        version: 'v2.11' 
     });
-  });
+    FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+            document.getElementById().innerHTML = 'Okay you are connected';
+        } else if (response.status === 'not_authorized') {
+            document.getElementById().innerHTML = 'You are not connected';
+        } else {
+            document.getElementById().innerHTML = 'Okay you are logged in to any facebook account';
+        }
+    });
+    FB.AppEvents.logPageView();
+}
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+function login() {
+    FB.login(function (response) {
+        if (response.status === 'connected') {
+            document.getElementById('demo').innerHTML = 'Okay you are connected';
+        } else if (response.status === 'not_authorized') {
+            document.getElementById('demo').innerHTML = 'You are not connected';
+        } else {
+            document.getElementById('demo').innerHTML = 'Okay you are logged in to any facebook account';
+        }
+    })
+}
+
+function info(){
+    FB.api('/me','GET',{fileds:'first_name,last_name_name_id'},function(response){
+        document.getElementById('demo').innerHTML=response.name;
+    });
+}
+
